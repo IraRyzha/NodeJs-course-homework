eb549ca8f4167f01c5947d57953449bd09b5bfa2
Ira Ryzha <ira.ryzha2803@gmail.com>
2024-10-30 13:48:47 +0200

    ._.

--- init.sql
+++ init.sql
@@ -1,22 +1,24 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE IF NOT EXISTS Users (
-    ID SERIAL PRIMARY KEY,
-    Name VARCHAR(100),
-    Email VARCHAR(100)
+    ID UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    Name VARCHAR(100)
 );
 
-CREATE TABLE IF NOT EXISTS UserPreferences (
-    PreferenceID SERIAL PRIMARY KEY,
-    Description VARCHAR(255),
-    LanguageCode VARCHAR(100),
-    UserId INT,
-    FOREIGN KEY (UserId) REFERENCES Users(ID) ON DELETE CASCADE
-);
+INSERT INTO Users (Name) VALUES ('ryzha');
+INSERT INTO Users (Name) VALUES ('other');
 
-INSERT INTO Users (ID, Name, Email) VALUES (1, 'ryzha', 'ryzha@');
-INSERT INTO Users (ID, Name, Email) VALUES (2, 'other', 'other@');
-INSERT INTO UserPreferences (PreferenceID, Description, LanguageCode, UserId) VALUES (1, '123', 'description', 1);
+CREATE OR REPLACE PROCEDURE add_user(p_user JSON)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    INSERT INTO Users (Name)
+    VALUES (
+        p_user->>'Name'
+    );
+END;
+$$;
 
--- DELETE FROM Users WHERE ID = 1;
-
--- ALTER TABLE users ADD UNIQUE (name)
--- ALTER TABLE users ALTER COLUMN name SET NOT NULL;
+CALL add_user('{"Name": "user1"}'::json);
+CALL add_user('{"Name": "user2"}'::json);
+CALL add_user('{"Name": "user3"}'::json);
