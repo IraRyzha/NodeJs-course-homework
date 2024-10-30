9f00bee2421d7a868a96891d833cfadc4d5b2075
Ira Ryzha <ira.ryzha2803@gmail.com>
2024-10-30 13:52:58 +0200

    ._.

--- init.sql
+++ init.sql
@@ -15,23 +15,8 @@ CREATE TABLE IF NOT EXISTS UserPreferences (
 INSERT INTO Users (ID, Name, Email) VALUES (1, 'ryzha', 'ryzha@');
 INSERT INTO Users (ID, Name, Email) VALUES (2, 'other', 'other@');
 INSERT INTO UserPreferences (PreferenceID, Description, LanguageCode, UserId) VALUES (1, '123', 'description', 1);
-
+ 
 -- DELETE FROM Users WHERE ID = 1;
-
-CREATE OR REPLACE PROCEDURE add_user(p_user JSON)
-LANGUAGE plpgsql
-AS $$
-BEGIN
-    INSERT INTO Users (ID, Name, Email)
-    VALUES (
-        (p_user->>'ID')::INT,
-        p_user->>'Name',
-        p_user->>'Email'
-    );
-END;
-$$;
-
-CALL add_user('{"ID": "3", "Name": "user", "Email": "email@"}'::json);
 
 -- ALTER TABLE users ADD UNIQUE (name)
 -- ALTER TABLE users ALTER COLUMN name SET NOT NULL;
