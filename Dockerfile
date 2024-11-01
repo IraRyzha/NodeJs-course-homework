ae82ad2ccf148291d5905237cfe47cf2613512eb
Ira Ryzha <ira.ryzha2803@gmail.com>
2024-11-01 12:58:50 +0200

    ._.

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,9 @@
-# Use the official Node.js image as a base image
-FROM node:16
+FROM node:14
 
-# Set the working directory inside the container
-WORKDIR /usr/src/app
+WORKDIR /app
 
-# Copy package.json and package-lock.json to the working directory
 COPY package*.json ./
-
-# Install the project dependencies
 RUN npm install
 
-# Copy the rest of the project files to the working directory
 COPY . .
-
-# Expose the port the app runs on
-EXPOSE 3000
-
-# Command to start the app
-CMD ["npm", "start"]
+CMD ["npm", "run", "dev"]
--- src/app.js
+++ src/app.js
@@ -1,14 +1,26 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const userController = require("./controllers/userController");
+require("./redisClient");
+
 const app = express();
-const userRoutes = require("./routes/userRoutes");
-const bodyParser = require("body-parser");
+const port = 3000;
 
-app.use(bodyParser.json());
-require("dotenv").config();
+app.use(express.json());
+mongoose.connect("mongodb://mongo:27017/userdb", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-app.use("/users", userRoutes);
+app.post("/user", userController.createUser);
+app.get("/user/:id", userController.getUser);
+app.put("/user", userController.updateUser);
+app.post("/comment", userController.createComment);
+app.get("/user/:id/comments", userController.getUserComments);
+app.post("/comments/create-comments", userController.createManyComments);
+app.put("/comments/update-comments", userController.updateManyComments);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(process.env.REDIS_URL);
+  console.log(`Server running at http://localhost:${port}`);
 });
