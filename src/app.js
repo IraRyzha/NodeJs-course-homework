1c0b446078eb11f3b0ae9fdbda116c3be0efd360
Ira Ryzha <ira.ryzha2803@gmail.com>
2024-11-15 14:11:52 +0200

    ._.

--- src/app.js
+++ src/app.js
@@ -32,3 +32,44 @@ console.log(generator2().next().value); // 2
 console.log(generator2().next().value); // 3
 
 console.log(generator2().next().value); // undefined
+
+// aditional task
+
+function sum(func) {
+  return function curried(...args) {
+    if (args.length >= func.length) {
+      return func(...args);
+    } else {
+      return (...nextArgs) => curried(...args, ...nextArgs);
+    }
+  };
+}
+
+function add(a, b, c, d) {
+  return a + b + c + d;
+}
+
+const curriedSum = sum(add);
+
+console.log(curriedSum(1)(2)(3)(4)); // 10
+
+// or
+
+function sum2(a) {
+  let currentSum = a;
+
+  function innerSum(b) {
+    currentSum += b;
+    return innerSum;
+  }
+
+  innerSum.valueOf = function () {
+    return currentSum;
+  };
+
+  return innerSum;
+}
+
+const a = sum2(1)(2)(3)(4).valueOf();
+
+console.log(a); // 10
